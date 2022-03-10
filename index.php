99c39270bce713910904579c830425422c609ffb
Blurmit <87097184+Blurmit@users.noreply.github.com>
2022-03-10 17:32:49 -0600

    Update index.php

--- index.php
+++ index.php
@@ -5,27 +5,27 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Blurmit</title>
-<link rel="icon" href="./assets/blurmit.png" class="main-image">
-<link href="./css/styles.css" rel="stylesheet">
+<link rel="icon" href="https://blurmit.ml/assets/blurmit.png" class="main-image">
+<link href="https://blurmit.ml/css/styles.css" rel="stylesheet">
 </head>
 
 <body>
 
 <div class="container" align="center">
-<img src="./assets/blurmit.png" class="main-img" draggable="false">
+<img src="https://blurmit.ml/assets/blurmit.png" class="main-img" draggable="false">
 <h1 class="text">Blurmit</h1>
 <p class="text">Java Developer</p>
 
 <a href="https://discord.com/users/676213665271119921">
-  <img src="./assets/discord.png" draggable="false" height="75" width="75" class="button-images">
+  <img src="https://blurmit.ml/assets/discord.png" draggable="false" height="75" width="75" class="button-images">
 </a>
 
 <a href="/experience">
-  <img src="./assets/profile.png" draggable="false" height="75" width="75" class="button-images">
+  <img src="https://blurmit.ml/assets/profile.png" draggable="false" height="75" width="75" class="button-images">
 </a>
 
 <a href="https://github.com/Blurmit">
-  <img src="./assets/github.png" draggable="false" height="75" width="75" class="button-images">
+  <img src="https://blurmit.ml/assets/github.png" draggable="false" height="75" width="75" class="button-images">
 </a>
 
 </div>
