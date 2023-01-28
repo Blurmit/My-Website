7e76a05cc81cd281a351874cdbeb76927aed3b4f
Blurmit <BlurmitDaBest@gmail.com>
2023-01-27 18:43:15 -0600

    MORE FIXES.

--- api/experience.php
+++ api/experience.php
@@ -22,7 +22,7 @@
     
   <div class="card" style="width:100%;max-width:300px;">
     <a href="https://www.advancius.net/">
-    <img src="./assets/advancius.png" style="width:100%;opacity:0.85">
+    <img src="../assets/advancius.png" style="width:100%;opacity:0.85">
       <div class="card-container">
       <p class="texty">Advancius Network</p>    
       <p class="gray-p">Advancius is a Minecraft network that offers a large variety of gamemodes. I was a general developer at Advancius. I mainly worked on updating gamemodes along with handling player reports and bug fixes.</p>    
@@ -31,7 +31,7 @@
   </div>
   <div class="card" style="width:100%;max-width:300px;">
     <a href="https://discord.gg/HSRsue3Mv4">
-    <img src="./assets/highlands.png" style="width:100%;opacity:0.85">
+    <img src="../assets/highlands.png" style="width:100%;opacity:0.85">
       <div class="card-container">
       <p class="texty">Highlands MMO</p>    
       <p class="gray-p">Highlands is a Minecraft MMO server. The server is still in quite a lot of development. During my time at highlands, I was the main quest &amp; API developer. The project has been put on pause until another owner has been found to take over.</p>    
@@ -39,7 +39,7 @@
     </a>
   </div>
   <div class="card" style="width:100%;max-width:300px;">
-    <img src="./assets/cystal.png" style="width:100%;opacity:0.85">
+    <img src="../assets/cystal.png" style="width:100%;opacity:0.85">
       <div class="card-container">
       <p class="texty">Crystal Cresent</p>    
       <p class="gray-p">Crystal Cresent is a Minecraft network that offers quite a selection of unique gamemodes. One of them being Cystal Caverns, which I was the main developer of. The server has since shutdown as the owners could not continue paying for it.</p>    
@@ -52,7 +52,7 @@
     
   <div class="card" style="width:100%;max-width:300px;">
     <a href="https://jpizza.tk/">
-    <img src="./assets/jpizza.png" style="width:100%;opacity:0.85">
+    <img src="../assets/jpizza.png" style="width:100%;opacity:0.85">
       <div class="card-container">
       <p class="texty">JPizza</p>    
       <p class="gray-p">JPizza is a programming language that is currently in development. JPizza is a language that mixes Python and Java. I am mainly developing the libraries and imports.</p>    
@@ -61,7 +61,7 @@
   </div>
   <div class="card" style="width:100%;max-width:300px;">
     <a href="https://discord.gg/JGbeMWnf8h">
-    <img src="./assets/shieldx.png" style="width:100%;opacity:0.85">
+    <img src="../assets/shieldx.png" style="width:100%;opacity:0.85">
       <div class="card-container">
       <p class="texty">ShieldX Manager</p>    
       <p class="gray-p">ShieldX manager was a very action packed Discord bot. It offered lots of creative and fun features including factions, wars, and an economy system. Unfortunately the bot has shutdown due to API complications.</p>    
@@ -70,7 +70,7 @@
   </div>
   <div class="card" style="width:100%;max-width:300px;">
     <a href="https://www.youtube.com/channel/UCK3fyDsGdDtfhDy0uq1FXCQ">
-    <img src="./assets/duomc.png" style="width:100%;opacity:0.85">
+    <img src="../assets/duomc.png" style="width:100%;opacity:0.85">
       <div class="card-container">
       <p class="texty">DuoMC</p>    
       <p class="gray-p">DuoMC is a Minecraft youtuber with around one thousand subscribers. I am the main manager/developer for his Minecraft server &amp; Discord.</p>    
